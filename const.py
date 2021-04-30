99595ee05fe6c412396854c9f433dc9cc4796592
Yaroslam <47461457+Yaroslam@users.noreply.github.com>
2021-04-30 23:20:02 +0300

    Add files via upload

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
-LANGUAGES = {'GNU C11': 'c', 'Clang++17 Diagnostics': 'cpp', 'GNU G++11': 'cpp', 'GNU G++': 'cpp',
-                     'GNU G++17': 'cpp', 'Microsoft Visual C++ 2010': 'cpp', 'Microsoft Visual C++ 2017': 'cpp', 'GNU G++17 9.2.0 (64 bit, msys 2)':'cpp',
-                     'C# 8, .NET Core 3.1': 'cs', 'C# Mono 6.8': 'cs', 'D DMD32 v2.091.0': 'd', 'Go 1.15.6': 'go',
-                     'Haskell GHC 8.10.1': 'hs', 'Java 11.0.6': 'java', 'Java 1.8.0_241': 'java', 'Kotlin 1.4.0': 'kt', 'OCaml 4.02.1': 'ml',
-                     'Delphi 7': 'pas', 'Free Pascal 3.0.2': 'pas', 'PascalABC.NET 3.4.2': 'pas', 'Perl 5.20.1': 'pl', 'PHP 7.2.13': 'php', 'Python 2.7.18': 'py', 'Python 3.9.1': 'py',
-                     'PyPy 2.7 (7.3.0)': 'py', 'PyPy 3.7 (7.3.0)': 'py', 'Ruby 3.0.0': 'rb', 'Rust 1.49.0': 'rs', 'Scala 2.12.8': 'sc', 'JavaScript V8 4.8.0': 'js', 'Node.js 12.6.3': 'js'}
+LANGUAGES = {'GNU C11': 'c', 'Clang++17 Diagnostics': 'cpp', 'GNU G++11': 'cpp', 'GNU G++14': 'cpp',
+                     'GNU G++17': 'cpp', 'MS C++': 'cpp', 'MS C++ 2017': 'cpp', 'GNU G++17(64)':'cpp',
+                     'NET Core C#': 'cs', 'Mono C#': 'cs', 'D': 'd', 'Go': 'go',
+                     'Haskell': 'hs', 'Java 11': 'java', 'Java 1.8': 'java', 'Kotlin': 'kt', 'OCaml': 'ml',
+                     'Delphi': 'pas', 'Free Pascal': 'pas', 'PascalABC.NET': 'pas', 'Perl': 'pl', 'PHP': 'php', 'Python 2': 'py', 'Python 3': 'py',
+                     'PyPy 2.7': 'py', 'PyPy 3.7': 'py', 'Ruby': 'rb', 'Rust': 'rs', 'Scala': 'sc', 'JavaScript': 'js', 'Node.js': 'js'}
